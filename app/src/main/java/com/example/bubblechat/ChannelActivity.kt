940419a815d45dcc76357b6e13441d18bf617025
Jeff Taggart <jeff@jetaggart.com>
2020-03-07 15:14:09 -0700

    Cleanup ChannelActivity

--- app/src/main/java/com/example/bubblechat/ChannelActivity.kt
+++ app/src/main/java/com/example/bubblechat/ChannelActivity.kt
@@ -13,27 +13,19 @@ import com.getstream.sdk.chat.view.MessageInputView
 import com.getstream.sdk.chat.viewmodel.ChannelViewModel
 import com.getstream.sdk.chat.viewmodel.ChannelViewModelFactory
 
-/**
- * Show the messages for a channel
- */
-class ChannelActivity : AppCompatActivity(),
-    MessageInputView.OpenCameraViewListener {
-
+class ChannelActivity : AppCompatActivity(), MessageInputView.OpenCameraViewListener {
     private var viewModel: ChannelViewModel? = null
     private var binding: ActivityChannelBinding? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // receive the intent and create a channel object
         val intent = intent
         val channelType = intent.getStringExtra(EXTRA_CHANNEL_TYPE)
         val channelID = intent.getStringExtra(EXTRA_CHANNEL_ID)
         val client = StreamChat.getInstance(application)
 
-        // we're using data binding in this example
         binding = DataBindingUtil.setContentView(this, R.layout.activity_channel)
-        // most the business logic of the chat is handled in the ChannelViewModel view model
         binding!!.lifecycleOwner = this
 
         val channel = client.channel(channelType, channelID)
@@ -42,10 +34,8 @@ class ChannelActivity : AppCompatActivity(),
             ChannelViewModelFactory(this.application, channel)
         ).get(ChannelViewModel::class.java)
 
-        // set listeners
         binding!!.messageInput.setOpenCameraViewListener(this)
 
-        // connect the view model
         binding!!.viewModel = viewModel
         binding!!.messageList.setViewHolderFactory(BubbleMessageViewHolderFactory())
         binding!!.messageList.setViewModel(viewModel!!, this)
@@ -70,9 +60,8 @@ class ChannelActivity : AppCompatActivity(),
     }
 
     companion object {
-
-        private val EXTRA_CHANNEL_TYPE = "com.example.chattutorial.CHANNEL_TYPE"
-        private val EXTRA_CHANNEL_ID = "com.example.chattutorial.CHANNEL_ID"
+        private val EXTRA_CHANNEL_TYPE = "com.example.bubblechat.CHANNEL_TYPE"
+        private val EXTRA_CHANNEL_ID = "com.example.bubblechat.CHANNEL_ID"
 
         fun newIntent(context: Context, channel: Channel): Intent {
             val intent = Intent(context, ChannelActivity::class.java)
